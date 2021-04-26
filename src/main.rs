b82c95baba71c567115ae71a8cc7017925eac855
Sankar boro <sankar.boro@yahoo.com>
2021-04-26 17:02:57 +0530

    Insertion sort bug fix. Functional and object oriented.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ Insertion Sort
 @author: Sankar boro
 */
 
-static CARDS: [usize; 7] = [20,100,34,55,76,5,100];
+static CARDS: [usize; 6] = [5, 2, 56, 6, 1, 3];
 
 struct Cards {
-    cards: [usize; 7],
+    cards: [usize; 6],
     pointer: usize,
     selected_card: usize,
-    compare_card: usize,
 }
 
 impl Cards {
@@ -26,47 +25,74 @@ impl Cards {
 
     fn change_compare_card(&mut self) {
         self.pointer -= 1;
-        self.compare_card = self.cards[self.pointer];
     }
 
     fn selected_card(&mut self) -> usize {
-        self.selected_card = self.cards[self.pointer + 1];
         self.selected_card
     }
 
+    fn set_selected_card(&mut self, index: usize) {
+        self.selected_card = self.cards[index];
+    }
+
     fn compare_card(&mut self) -> usize {
-        self.compare_card = self.cards[self.pointer];
-        self.compare_card
+        self.cards[self.pointer]
     }
 
     fn swap_card(&mut self) {
-        self.cards[self.pointer + 1] = self.compare_card;
+        self.cards[self.pointer + 1] = self.cards[self.pointer];
         self.cards[self.pointer] = self.selected_card;
     }
 }
 
-fn main() {
+fn functional() {
+    let mut cards = CARDS;
+
+    for j in 1..cards.len() {
+        let sc = cards[j];
+        let mut pointer = j - 1;
+
+        while pointer > 0 && cards[pointer] > cards[pointer + 1] {
+            cards[pointer + 1] = cards[pointer];
+            cards[pointer] = sc;
+            pointer -= 1;
+        }
+
+        if pointer == 0 && cards[pointer] > cards[pointer + 1] {
+            cards[pointer + 1] = cards[pointer];
+            cards[pointer] = sc;
+        }
+    }
+    
+    println!("{:?}", cards);
+}
+
+fn object_oriented() {
+
     let mut cards = Cards {
         cards: CARDS,
         selected_card: 0,
-        compare_card: 0,
         pointer: 0,
     };
 
     for index in 1..cards.cards.len() {
+        cards.set_selected_card(index);
         cards.set_pointer(index);
-        let selected_card = cards.selected_card();
-        let compare_card = cards.compare_card();
 
-        while cards.get_pointer() > 0 && compare_card > selected_card {
+        while cards.get_pointer() > 0 && cards.compare_card() > cards.selected_card() {
             cards.swap_card();
             cards.change_compare_card();
         }
 
-        if cards.get_pointer() == 0 && compare_card > selected_card {
+        if cards.get_pointer() == 0 && cards.compare_card() > cards.selected_card() {
             cards.swap_card();
         }
     }
 
-    println!("Cards: {:?}", cards.cards);
+    println!("{:?}", cards.cards);
+}
+
+fn main() {
+    object_oriented();
+    functional();
 }
